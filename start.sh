9df3ae60d7c7862045418c7387cfffa254aa8bb8
Slesarev <slesarew@gmail.com>
2024-04-16 18:06:22 +0300

    fix: update starter script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 KALATORI_HOST="127.0.0.1:16726" \
-KALATORI_SEED="" \
-KALATORI_DECIMALS="12" \
-KALATORI_DESTINATION="0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d" \
-KALATORI_LOG="subxt::events::events_type=off" \
+KALATORI_SEED="bottom drive obey lake curtain smoke basket hold race lonely fit walk" \
+KALATORI_DATABASE="database-ah-usdc.redb" \
+KALATORI_RPC="wss://polkadot-asset-hub-rpc.polkadot.io" \
+KALATORI_RECIPIENT="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY" \
+KALATORI_USD_ASSET="USDC" \
 cargo r
+
