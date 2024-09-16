71dcd2226824f0491f424aaf13da2e9b14f8549e
Slesarew <33295157+Slesarew@users.noreply.github.com>
2024-09-16 23:55:54 +0300

    Update src/server.rs
    Co-authored-by: Vova Lando <vova.lando@gmail.com>

--- src/server.rs
+++ src/server.rs
@@ -95,9 +95,6 @@ async fn process_order(
             .parse()
             .map_err(|_| OrderError::InvalidParameter(AMOUNT.into()))?;
 
-        if currency != "USDC" {
-            return Err(OrderError::UnknownCurrency);
-        }
 
         if amount < 0.07 {
             return Err(OrderError::LessThanExistentialDeposit(0.07));
