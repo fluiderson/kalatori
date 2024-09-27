f299f017d10d97f501e0d96eeb6cb438d938f7a0
Vova Lando <vova.lando@gmail.com>
2024-09-27 15:36:51 +0300

    fix:get order handler functionality part

--- src/handlers/order.rs
+++ src/handlers/order.rs
@@ -1,4 +1,3 @@
-use std::f32::consts::E;
 use axum::{
     extract::{Path, State as ExtractState},
     response::{IntoResponse, Response},
@@ -8,10 +7,11 @@ use axum::{
 use crate::{
     state::State,
     definitions::api_v2::{OrderQuery, OrderResponse, InvalidParameter, AMOUNT, CURRENCY, OrderStatus},
-    error::{OrderError, ForceWithdrawalError},
+    error::{Error, OrderError, ForceWithdrawalError},
 };
 use serde::Deserialize;
 
+const EXISTENTIAL_DEPOSIT: f64 = 0.07;
 #[derive(Debug, Deserialize)]
 pub struct OrderPayload {
     pub amount: Option<f64>,
@@ -22,46 +22,49 @@ pub struct OrderPayload {
 pub async fn process_order(
     state: State,
     order_id: String,
-    payload: OrderPayload,
+    payload: Option<OrderPayload>,
 ) -> Result<OrderResponse, OrderError> {
-    if payload.amount.is_none() && payload.currency.is_none() && payload.callback.is_none() {
-        return Err(OrderError::MissingParameter(AMOUNT.to_string()));
-    }
-    // AMOUNT
-    if payload.amount.is_none() {
-        return Err(OrderError::MissingParameter(AMOUNT.to_string()));
-    } else if payload.amount.unwrap() < EXISTENTIAL_DEPOSIT {
-        return Err(OrderError::LessThanExistentialDeposit(EXISTENTIAL_DEPOSIT));
-    }
-
-    // CURRENCY
-    if payload.currency.is_none() {
-        return Err(OrderError::MissingParameter(CURRENCY.to_string()));
-    } else {
-        let currency = payload.currency.clone().unwrap();
-        if !state.is_currency_supported(&currency)
-            .await
-            .map_err(|_| OrderError::InternalError)? {
-            return Err(OrderError::UnknownCurrency);
+    if let Some(payload) = payload {
+        // AMOUNT validation
+        if payload.amount.is_none() {
+            return Err(OrderError::MissingParameter(AMOUNT.to_string()));
+        } else if payload.amount.unwrap() < EXISTENTIAL_DEPOSIT {
+            return Err(OrderError::LessThanExistentialDeposit(EXISTENTIAL_DEPOSIT));
         }
-    }
 
-    state
-        .create_order(OrderQuery {
-            order: order_id,
-            amount: payload.amount.unwrap(),
-            callback: payload.callback.unwrap_or_default(),
-            currency: payload.currency.unwrap(),
-        })
-        .await
-        .map_err(|_| OrderError::InternalError)
+        // CURRENCY validation
+        if payload.currency.is_none() {
+            return Err(OrderError::MissingParameter(CURRENCY.to_string()));
+        } else {
+            let currency = payload.currency.clone().unwrap();
+            if !state.is_currency_supported(&currency)
+                .await
+                .map_err(|_| OrderError::InternalError)? {
+                return Err(OrderError::UnknownCurrency);
+            }
+        }
+
+        state
+            .create_order(OrderQuery {
+                order: order_id,
+                amount: payload.amount.unwrap(),
+                callback: payload.callback.unwrap_or_default(),
+                currency: payload.currency.unwrap(),
+            })
+            .await
+            .map_err(|_| OrderError::InternalError)
+    } else {
+        return state.order_status(&order_id).await.map_err(|_| OrderError::InternalError);
+    }
 }
+
 
 pub async fn order(
     ExtractState(state): ExtractState<State>,
     Path(order_id): Path<String>,
-    Json(payload): Json<OrderPayload>,
+    payload: Option<Json<OrderPayload>>,
 ) -> Response {
+    let payload = payload.map(|p| p.0);
     match process_order(state, order_id, payload).await {
         Ok(order) => match order {
             OrderResponse::NewOrder(order_status) => (StatusCode::CREATED, Json(order_status)).into_response(),
