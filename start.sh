2de389166db8bc06ec0df39405b0908b73198059
Vova Lando <vova.lando@gmail.com>
2024-11-14 22:30:18 +0200

    chore: added tests

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 KALATORI_HOST="127.0.0.1:16726" \
+KALATORI_CONFIG="configs/chopsticks.toml" \
 KALATORI_SEED="bottom drive obey lake curtain smoke basket hold race lonely fit walk" \
 KALATORI_RECIPIENT="5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY" \
 KALATORI_REMARK="test" \
